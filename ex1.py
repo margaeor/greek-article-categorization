160fc686b1c9d24676bf586249c1a5586b000cf4
George Margaritis <margaeor@gmail.com>
2019-05-24 04:03:47 +0300

    Main Phase

--- ex1.py
+++ ex1.py
@@ -1,23 +1,69 @@
 from preprocessor import Preprocessor
+import numpy as np
 
 
 
 prep = Preprocessor()
 
-articles, labels = prep.parse_files('train')
+# Preprocess
+print("Preprocessing...")
+articles_train, labels_train = prep.parse_files('train')
+articles_test, labels_test = prep.parse_files('test')
 
-articles = articles[:100]
 
-dct = prep.create_word_dictionary(articles)
 
-print(dct)
 
-tfidf = prep.create_tfidf(dct,articles)
+#articles_train,labels_train = articles_train[:100],labels_train[:100]
+#articles_test,labels_test = articles_test[:100],labels_test[:100]
 
-print(prep.transform(tfidf))
+# Create dictionary
+print("Creating Dictionary...")
+dct = prep.create_word_dictionary(articles_train)
+
+
+# Create tfidf table
+print("Creating tfidf matrix...")
+X_train = prep.create_tfidf_train(dct,articles_train)
+X_test = prep.create_tfidf_test(dct,articles_test)
+
+# Tranform data
+print("Transforming data...")
+X_train = prep.transform_train(X_train,method='entropy')
+X_test = prep.transform_test(X_test)
+
+# Reduce dimensions
+print("Reducing dimensions...")
+#X_train = prep.reduce_dims_train(X_train,labels_train,'LDA',n_components=50,solver='svd')
+X_train = prep.reduce_dims_train(X_train,labels_train,'PCA',n_components=50)
+X_test = prep.reduce_dims_test(X_test)
+
+
+
+
+#print(X_train.shape)
+
+
+
+# Encode labels
+print("Decoding Labels...")
+y_train = labels_train
+y_test = labels_test
+y_train = prep.encode_labels(labels_train)
+y_test = prep.encode_labels(labels_test)
+
+
+
+print("Training Model...")
+#prep.train_model(X_train,y_train,method='KNN',n_neighbors=5)
+#prep.train_model(X_train,y_train,method='NB')
+#prep.train_model(X_train,y_train,method='GMM',n_components=5,init_params='kmeans')
+#prep.train_model(X_train,y_train,method='RandomForest')
+prep.train_model(X_train,y_train,method='ANN',layers=[(10,'relu'),(20,'relu')])
+
+print("Accuracy: ",prep.evaluate_model(X_test,y_test))
+
+#print(X.shape)
+
+#print(prep.transform(tfidf))
 
 #print(tfidf.shape)
-
-
-
-#print(articles)
--- preprocessor/__init__.py
+++ preprocessor/__init__.py
@@ -4,6 +4,22 @@ import os
 import codecs
 import pickle
 import numpy as np
+from keras import losses
+import sklearn
+
+from numpy import inf
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.ensemble import RandomForestClassifier as RFC
+
+from sklearn import decomposition
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.naive_bayes import GaussianNB
+from sklearn.mixture import GaussianMixture as GMM
 
 from greek_stemmer import GreekStemmer
 
@@ -20,24 +36,12 @@ class Preprocessor(object):
 		#print(self.neutral_words)
 
 		self.greek_stemmer = GreekStemmer()
-
-
-
-
-	def preprocess(self,text):
-
-		text = self.strip_accents(text)
-
-		text = text.upper()
-
-		words = self.tokenize(text)
-
-		words = [self.greek_stemmer.stem(w) for w in words if w not in self.neutral_words]
-
-		words = [w for w in words if len(w)>0]
-
-		return words
-
+		self.reduction_model = None
+		self.transform_model = None
+		self.classifier = None
+		self.label_dict = None
+		self.classifier_type = None
+		self.idf = []
 
 	def parse_files(self,dir,only_parse=False):
 
@@ -78,6 +82,22 @@ class Preprocessor(object):
 		return articles, labels
 
 
+	def preprocess(self,text):
+
+		text = self.strip_accents(text)
+
+		text = text.upper()
+
+		words = self.tokenize(text)
+
+		words = [self.greek_stemmer.stem(w) for w in words if w not in self.neutral_words]
+
+		words = [w for w in words if len(w)>0]
+
+		return words
+
+
+
 
 	def strip_accents(self, s):
 		return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
@@ -92,53 +112,6 @@ class Preprocessor(object):
 		words = text.split(' ')
 
 		return words
-
-
-	def create_tfidf(self,word_dict,texts):
-
-		n_words = max(word_dict.values())
-		n_documents = len(texts)
-
-		m = []
-
-		for text in texts:
-			word_vec = [0]*(n_words+1)
-
-			for word in text:
-
-				word_vec[word_dict[word]] +=1
-
-			m.append(word_vec)
-
-
-		m = np.array(m)
-
-		tft = m/np.sum(m,axis=1).reshape((-1,1))
-
-		idf = np.log(n_documents/np.sum(np.int32(m>0),axis=0))
-
-		#print(tft.shape)
-		#print(idf.shape)
-
-		tfidf = tft*idf
-
-		#print(np.min(tfidf))
-
-		return tfidf
-
-	def transform(self,tfidf,method='entropy'):
-
-		if method == 'entropy':
-
-			p =  tfidf / np.sum(tfidf,axis=0)
-			p[np.isnan(p)] = 1
-
-			e = 1 + np.nan_to_num(np.sum(p*np.log(p),axis=0)/np.log(tfidf.shape[0]))
-
-			l = e*np.log(1 + tfidf)
-
-			return l
-
 
 
 	def create_word_dictionary(self,texts,recreate=True):
@@ -165,3 +138,272 @@ class Preprocessor(object):
 			pickle.dump(words_dict, f, pickle.HIGHEST_PROTOCOL)
 
 		return words_dict
+
+
+	def create_tfidf_train(self,word_dict,texts):
+
+		n_words = max(word_dict.values())
+		n_documents = len(texts)
+
+		m = []
+
+		for text in texts:
+			word_vec = [0]*(n_words+1)
+
+			for word in text:
+
+				if word in word_dict:
+					word_vec[word_dict[word]] +=1
+
+			m.append(word_vec)
+
+
+		m = np.array(m)
+
+		tft = m/np.sum(m,axis=1).reshape((-1,1))
+
+		idf = np.log(n_documents/np.sum(np.int32(m>0),axis=0))
+
+		self.idf = idf
+
+		tfidf = tft*idf
+
+		return tfidf
+
+	def create_tfidf_test(self, word_dict, texts):
+
+		if len(self.idf) == 0:
+			raise Exception("You must create training idf first")
+
+		n_words = max(word_dict.values())
+		n_documents = len(texts)
+
+		m = []
+
+		for text in texts:
+			word_vec = [0] * (n_words + 1)
+
+			for word in text:
+
+				if word in word_dict:
+					word_vec[word_dict[word]] += 1
+
+			m.append(word_vec)
+
+		m = np.array(m)
+
+		tft = m / np.sum(m, axis=1).reshape((-1, 1))
+
+		tfidf = tft * self.idf
+
+		return tfidf
+
+	def transform_train(self,tfidf,method='entropy'):
+
+		self.transform_model = method
+
+		pickle_file = './data/train/tranform.pickle'
+
+
+		#if os.path.isfile(pickle_file):
+		#	with open(pickle_file, 'rb') as f:
+		#		# The protocol version used is detected automatically, so we do not
+		#		# have to specify it.
+		#		return pickle.load(f)
+
+		l = []
+		if method == 'entropy':
+
+			p =  tfidf / np.sum(tfidf,axis=0)
+			p[np.isnan(p)] = 1
+
+			e = 1 + np.nan_to_num(np.sum(p*np.log(p),axis=0)/np.log(tfidf.shape[0]))
+			print(e.shape)
+			e[e== -inf] = 0
+
+			l = e*np.log(1 + tfidf)
+
+			#return l
+
+		elif method == 'binary':
+
+			l = 1*(tfidf > 0)
+			#return 1*(tfidf > 0)
+
+		elif method == 'log':
+			l = np.log(1+tfidf)
+			#return np.log(1+tfidf)
+
+		with open(pickle_file, 'wb') as f:
+			# Pickle the 'data' dictionary using the highest protocol available.
+			pickle.dump(l, f, pickle.HIGHEST_PROTOCOL)
+
+		return l
+
+	def transform_test(self,tfidf):
+
+		if self.transform_model == None:
+			raise Exception("You must train first!")
+
+		return self.transform_train(tfidf,self.transform_model)
+
+
+	def reduce_dims_train(self,X,y,method='PCA',**kwargs):
+
+		pickle_file = './data/train/reduced_dims.pickle'
+
+
+		#if os.path.isfile(pickle_file):
+		#	with open(pickle_file, 'rb') as f:
+		#		# The protocol version used is detected automatically, so we do not
+				# have to specify it.
+		#		return pickle.load(f)
+
+		if method == 'PCA':
+			# n_components = 50
+			self.reduction_model = decomposition.PCA(**kwargs)
+
+
+		elif method == 'LDA':
+			# n_components=50, solver=svd
+			self.reduction_model = LDA(**kwargs)
+
+		elif method == 'MEAN':
+			# n_components=50
+			self.reduction_model = MeanReduction(**kwargs)
+
+		else:
+			raise Exception("Wrong Method")
+
+
+		self.reduction_model.fit(X, y)
+		transformed = self.reduction_model.transform(X)
+
+		#with open(pickle_file, 'wb') as f:
+		#	# Pickle the 'data' dictionary using the highest protocol available.
+		#	pickle.dump(transformed, f, pickle.HIGHEST_PROTOCOL)
+
+		return transformed
+
+
+	def reduce_dims_test(self,X):
+
+		if self.reduction_model == None:
+			raise Exception("You must train first!")
+
+		return self.reduction_model.transform(X)
+
+	def encode_labels(self,y):
+
+		s = set(y)
+
+		n = len(s)
+
+		if self.label_dict == None:
+			self.label_dict = {l: [1 * (i == l) for l in range(n)] for (i, l) in enumerate(s)}
+
+		y = np.array([self.label_dict[label] for label in y])
+
+		return y
+
+
+	def train_model(self,X,y,method='KNN',**kwargs):
+
+		self.classifier_type = method
+		#85%
+		if method == 'KNN':
+			# n_neighbors=5, metric='minkowski'
+			self.classifier = KNN(**kwargs)
+			self.classifier.fit(X, y)
+		#77%
+		elif method == 'NB':
+			self.classifier = GaussianNB(**kwargs)
+			self.classifier.fit(X, y)
+
+		elif method == 'GMM':
+			# n_components=5, tol=1e-3, max_iter=100, init_params='kmeans'
+			self.classifier = GMM(**kwargs)
+			self.classifier.fit(X, y)
+		#79%
+		elif method == 'RandomForest':
+			self.classifier = RFC(**kwargs)
+			self.classifier.fit(X, y)
+
+		elif method == 'LDA':
+			#n_components=5, learning_method='batch'|'online'
+			self.classifier = LatentDirichletAllocation(**kwargs)
+			self.classifier.fit(X, y)
+
+		elif method == 'ANN':
+
+			self.classifier = Sequential()
+			l1,a1 = 12,'relu'
+			l2,a2 = 8,'relu'
+			n_epochs = 150
+			b_size = 10
+
+			if 'layers' in kwargs:
+				l1,a1 = kwargs['layers'][0]
+				l2,a2 = kwargs['layers'][1]
+
+			if 'epochs' in kwargs:
+				n_epochs = kwargs['epochs']
+
+			if 'batch_size' in kwargs:
+				b_size = kwargs['batch_size']
+
+			self.classifier.add(Dense(l1, input_dim=X.shape[1], activation=a1))
+			self.classifier.add(Dense(l2, activation=a2))
+			self.classifier.add(Dense(y.shape[1], activation='softmax'))
+
+			self.classifier.compile(loss=losses.kullback_leibler_divergence, optimizer='adam', metrics=['accuracy'])
+			self.classifier.fit(X, y, epochs = n_epochs , batch_size = b_size )
+
+		else:
+			raise Exception("No such classifier exists!")
+
+
+
+	def evaluate_model(self,X,y):
+
+		if self.classifier==None or self.classifier_type == None:
+			raise Exception("You must first train the classifier!")
+
+		if self.classifier_type == 'ANN':
+			loss,acc = self.classifier.evaluate(X, y)
+
+			return acc
+
+		#elif self.classifier_type == 'LDA':
+		#	self.classifier.predict()
+		elif self.classifier_type == 'NB' or self.classifier_type == 'GMM':
+			#print(pred.shape)
+			pred = self.classifier.predict(X)
+			return np.sum(1*(pred == y))/len(y)
+		else:
+			pred = np.argmax(self.classifier.predict(X),axis=1)
+			y = np.argmax(y,axis=1)
+			print(pred.shape)
+			return np.sum(1*(pred == y))/y.shape[0]
+
+	#def pickle_data(self,file,object):
+	#
+
+
+class MeanReduction:
+
+	def __init__(self,**kwargs):
+		self.components = []
+		self.n_components = kwargs['n_components'] if 'n_components' in kwargs else 50
+
+	def fit(self,X,y):
+
+		means = np.mean(X,axis=0)
+		self.components = means.argsort()[::-1][:self.n_components]
+
+	def transform(self,X):
+
+		if len(self.components) == 0:
+			raise Exception("You must train first!")
+
+		return X[:,self.components]
